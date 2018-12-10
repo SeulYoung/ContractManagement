e2d6b9f3f6a44b2fc02ffbc3dcb6ada6f9b3cd4f
lixingyuan1998 <38291879+lixingyuan1998@users.noreply.github.com>
2018-12-10 22:13:45 +0800

    customer add

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.landing, name='landing'),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('profile.html', views.profile, name='profile'),
     path('emailUpdate.html', views.email_update, name='email_update'),
     path('passwordUpdate.html', views.password_update, name='password_update'),
+    url(r'^customer_add/$', views.customer_add.as_view(), name='customer_add'),
+    url(r'^customer_modify/$', views.customer_modify.as_view(), name='customer_modify'),
+    url(r'^customer_select/$', views.customer_select.as_view(), name='customer_select'),
+    url(r'^customer_delete/$', views.customer_delete.as_view(), name='customer_delete'),
 ]
