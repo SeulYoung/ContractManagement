569b2c5ec1cf763b6653a326b62dde1b83607d0e
wkz <742955703@qq.com>
2018-12-22 20:48:42 +0800

    角色管理，权限管理，分配合同基本完成。

--- app/SystemManagement.py
+++ app/SystemManagement.py
@@ -1,26 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import *
 from django.contrib.auth.models import User
 import django.utils.timezone as timezone
 
 
 def wcon_sel(request):
+    out_list = []
     if request.method == "POST":
-        s_num = request.POST.get('num')
-        if s_num:
-            con_list = State.objects.filter(conNum=s_num, type=1)
+        s_con = request.POST.get('s_con')
+        if s_con:
+            c_list = Contract.objects.filter(name=s_con).first()
+            if c_list:
+                con_list = State.objects.filter(conNum=c_list.num).first()
+                isexist = Process.objects.filter(conNum=c_list.num)
+                if not isexist:
+                    out_list.append({'conName': s_con, 'time': con_list.time})
         else:
-            con_list = State.objects.filter(type=1)
-        return render(request, 'Wcontract_sel.html', {'wcon_list': con_list})
+            state_list = State.objects.filter(type=1)
+            for user in state_list:
+                c_list = Contract.objects.filter(num=user.conNum).first()
+                isexist = Process.objects.filter(conNum=user.conNum)
+                if not isexist:
+                    out_list.append({'conName': c_list.name, 'time': user.time})
+        return render(request, 'Wcontract_sel.html', {'wcon_list': out_list})
 
-    con_list = State.objects.filter(type=1)
-    return render(request, 'Wcontract_sel.html', {'wcon_list': con_list})
+    state_list = State.objects.filter(type=1)
+    for user in state_list:
+        c_list = Contract.objects.filter(num=user.conNum).first()
+        isexist = Process.objects.filter(conNum=user.conNum)
+        if not isexist:
+            out_list.append({'conName': c_list.name, 'time': user.time})
+
+    return render(request, 'Wcontract_sel.html', {'wcon_list': out_list})
 
 
 def con_assign(request):
     if request.method == "POST":
-        num = request.POST.get('conNum')
-        contract_list = Contract.objects.get(num=num)
+        con_name = request.POST.get('conName')
+        num_list = Contract.objects.filter(name=con_name).first()
 
         countersignp = request.POST.getlist('check_box_list1')
         approvalp = request.POST.getlist('check_box_list2')
@@ -30,16 +48,12 @@ def con_assign(request):
             role_list2 = Right.objects.filter(description__contains='审批合同')
             role_list3 = Right.objects.filter(description__contains='签订合同')
             return render(request, 'contract_assign.html',{'role_list1': role_list1,'role_list2': role_list2,
-                                                           'role_list3': role_list3, 'con_list': contract_list})
-
+                                                           'role_list3': role_list3, 'conName': con_name})
         else:
-            State.objects.filter(conNum=num).update(type=6)
-            Process.objects.create(conNum=num, type=1, state=0, userName=countersignp, time=timezone.now())
-            Process.objects.create(conNum=num, type=2, state=0, userName=approvalp, time=timezone.now())
-            Process.objects.create(conNum=num, type=3, state=0, userName=signp, time=timezone.now())
-
-    con_list = State.objects.filter(type=1)
-    return render(request, 'Wcontract_sel.html', {'wcon_list': con_list})
+            Process.objects.create(conNum=num_list.num, type=1, state=0, userName=countersignp, time=timezone.now())
+            Process.objects.create(conNum=num_list.num, type=2, state=0, userName=approvalp, time=timezone.now())
+            Process.objects.create(conNum=num_list.num, type=3, state=0, userName=signp, time=timezone.now())
+            return HttpResponseRedirect('Wcontract_sel.html')
 
 
 def wper_sel(request):
@@ -53,20 +67,18 @@ def wper_sel(request):
             else:
                 right_list = Right.objects.filter(userName=user_list.username).first()
                 if right_list:
-                    out_list.append({'username': user_list.username, 'rolename': right_list.userName})
+                    out_list.append({'username': user_list.username, 'rolename': right_list.roleName})
                 else:
                     out_list.append({'username': user_list.username, 'rolename': '无'})
                 return render(request, 'Wpermission_sel.html', {'out_list': out_list})
 
     user_list = User.objects.all()
     for user in user_list:
-        right_list= Right.objects.filter(userName=user.username).first()
+        right_list = Right.objects.filter(userName=user.username).first()
         if right_list:
-            out_list.append({'username':user.username, 'rolename':right_list.userName})
+            out_list.append({'username': user.username, 'rolename': right_list.roleName})
         else:
             out_list.append({'username': user.username, 'rolename': '无'})
-
-
     return render(request, 'Wpermission_sel.html', {'out_list': out_list})
 
 
@@ -77,11 +89,11 @@ def permission_assign(request):
         if permission:
             role_msg = Role.objects.filter(name = permission).first()
             Right.objects.create(userName = u_name, roleName = permission, description = role_msg.functions)
-
-            right_list = Right.objects.all()
-            user_list = User.objects.all()
-            return render(request, 'Wpermission_sel.html', {'right_list': right_list, 'user_list': user_list})
+            return HttpResponseRedirect('Wpermission_sel.html')
         else:
+            right_list = Right.objects.filter(userName=u_name).first()
+            if right_list:
+                return HttpResponseRedirect('Wpermission_sel.html')
             role_list = Role.objects.all()
             return render(request, 'permission_assign.html', {'user_w': u_name, 'role_list': role_list})
 
@@ -123,9 +135,7 @@ def role_add(request):
                     return render(request, 'role_add.html', {'r_error': '该角色已存在，请重新输入'})
 
             Role.objects.create(name = role_name, description = description, functions = permission)
-
-            role_list = Role.objects.all()
-            return render(request, 'role_sel.html', {'role_list': role_list})
+            return HttpResponseRedirect("role_sel.html")
 
     return render(request, 'role_add.html')
 
@@ -152,6 +162,17 @@ def role_del(request):
 
 
 def role_mod(request):
+    if request.method == 'POST':
+        role_name = request.POST.get('m_name')
+        description = request.POST.get('description')
+        permission = request.POST.getlist('check_box_list')
+        if not description:
+            role_list = Role.objects.all()
+            return render(request, 'role_mod.html', {'m_name': role_name, 'role_list': role_list, 'mod':"mod"})
+        else:
+            Role.objects.filter(name=role_name).update(description=description, functions=permission)
+            return HttpResponseRedirect("role_sel.html")
 
-    return render(request, 'role_mod.html')
+    role_list = Role.objects.all()
+    return render(request, 'role_mod.html', {'role_list': role_list})
 
